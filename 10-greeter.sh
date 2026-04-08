0cb301fc109b64f8a3a8f9e867752c717efb6d67
lsaeed1 <lsaeed@ggc.edu>
2026-04-08 12:18:15 -0400

    Parallel universe edits to 10-greeter.sh

--- 10-greeter.sh
+++ 10-greeter.sh
@@ -27,3 +27,24 @@ fi
 # exercise: Add a second argument to the function 
 # and make it give a greeting based on the new 
 # argument.
+
+function weekend_greet2
+{
+   echo "Enjoy the weekend, ${1}! Hope you get to eat some ${2}!"
+}
+
+function weekday_greet2
+{
+   echo "Hope you are working hard, ${1}! Treat yourself to some ${2} later!"
+}
+
+echo "Please enter your favorite food"
+read food
+
+day=$(date | cut -d' ' -f1)
+
+if [ $day = "Sat" ] || [ $day = "Sun" ]; then
+   weekend_greet2 $name $food
+else
+   weekday_greet2 $name $food
+fi
